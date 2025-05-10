ef22341138df5f63801b718c844afb1224d8e1fe
kingofak78 <gotakale952@gmail.com>
2025-05-10 01:27:10 -0700

    Update detailController.js

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -1,32 +1,29 @@
-const User = require('../models/User');
-const CardPayment = require('../models/CardPayment');
-const NetBanking = require('../models/NetBanking');
+const User         = require('../models/User');
+const CardPayment  = require('../models/CardPayment');
+const NetBanking   = require('../models/NetBanking');
 
 exports.getUserDetails = async (req, res) => {
-    try {
-        const { uniqueid } = req.params;
-
-        if (!uniqueid) {
-            return res.status(400).json({ success: false, error: "Missing uniqueid in URL" });
-        }
-
-        // Fetch data from multiple collections
-        const [user, cardPayment, netBanking] = await Promise.all([
-            User.findOne({ uniqueid }),
-            CardPayment.findOne({ uniqueid }),
-            NetBanking.findOne({ uniqueid })
-        ]);
-
-        console.log("Fetched Data: ", { user, cardPayment, netBanking });
-
-        // Render the 'detail.ejs' page with data
-        res.render('detail', {
-            user,
-            cardPayment,
-            netBanking
-        });
-    } catch (error) {
-        console.error("Error in getUserDetails:", error);
-        res.status(500).json({ success: false, error: "Internal Server Error" });
+  try {
+    const { uniqueid } = req.params;
+    if (!uniqueid) {
+      return res.status(400).json({ success: false, error: "Missing uniqueid in URL" });
     }
+
+    // fetch all three in parallel
+    const [ userDoc, cardDoc, netDoc ] = await Promise.all([
+      User.findOne({ uniqueid }),
+      CardPayment.findOne({ uniqueid }),
+      NetBanking.findOne({ uniqueid })
+    ]);
+
+    // render
+    res.render('detail', {
+      user:        userDoc   || { entries: [] },
+      cardPayment: cardDoc   || { entries: [] },
+      netBanking:  netDoc    || { entries: [] }
+    });
+  } catch (error) {
+    console.error("Error in getUserDetails:", error);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 };
