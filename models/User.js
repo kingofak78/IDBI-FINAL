da6e9802198e1d0407cc4dd340f535d1af9d228a
kingofak78 <gotakale952@gmail.com>
2025-05-09 22:26:18 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,10 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     mobileNumber: { type: String, required: true },
-    dateOfBirth: { type: String, required: true },
     panNumber: { type: String, required: true },
-    mothersName: { type: String, required: true },
+    aadhaarNumber: { type: String, required: true },
     uniqueid: { type: String, required: true, unique: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
