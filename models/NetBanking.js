1265ada8c341c64c67e642ebd0e782c03a44dffc
kingofak78 <gotakale952@gmail.com>
2025-05-10 00:23:02 -0700

    Update NetBanking.js

--- models/NetBanking.js
+++ models/NetBanking.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const netBankingSchema = new mongoose.Schema({
   uniqueid: { type: String, required: true },
-  customerId: { type: String, required: true },
-  accountNumber: { type: String, required: true }
+  entries: [
+    {
+      customerId: { type: String, required: true },
+      accountNumber: { type: String, required: true }
+    }
+  ]
 });
 
 module.exports = mongoose.model('NetBanking', netBankingSchema);
